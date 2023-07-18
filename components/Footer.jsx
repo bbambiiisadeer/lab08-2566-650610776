93b7ea3c4ca9b2d42f0b99140f2c34c35e730515
bbam <nunnapats1822@gmail.com>
2023-07-18 17:17:33 +0700

    completed

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,7 +2,7 @@ export const Footer = (props) => {
   return (
     <div className="mt-3">
       <p className="text-secondary text-center">
-        copyright @ {props.year} {props.fullname} {props.studentid}
+        copyright © {props.year} {props.fullname} {props.studentid}
       </p>
     </div>
   );
